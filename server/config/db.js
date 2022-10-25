598adef908a5828cb489e0624a3825b320f8fc25
charliedbeing <zding12@my.centennialcollege.ca>
2022-10-25 11:34:20 -0400

    change mongodb connection

--- server/config/db.js
+++ server/config/db.js
@@ -7,5 +7,5 @@
 
 module.exports = {
   //local MongoDB deployment ->
-  URI:"mongodb+srv://charlie:password301159548@cluster0.xlugz0a.mongodb.net/faculty_info",
+  URI:"mongodb+srv://charlie:A368Hdouw79glmvv@mongodbserver.tmdzgbi.mongodb.net/faculty_inf?retryWrites=true&w=majority"
 };
